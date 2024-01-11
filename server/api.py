f2300164cc7a6bd9a759bffcab784fe088310272
Tal Alon <talalon369@gmail.com>
2024-01-11 14:02:04 +0200

    fix cors

--- server/api.py
+++ server/api.py
@@ -7,16 +7,9 @@ from s_lang import run
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
